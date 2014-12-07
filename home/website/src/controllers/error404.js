a0f32f7e57227ecb9f5cc92c232ae14ec01e44f5
emixam150 <emixam150@gmail.com>
2014-12-07 20:37:05 +0100

    maj de la page 404

--- home/website/src/controllers/error404.js
+++ home/website/src/controllers/error404.js
@@ -1,12 +1,42 @@
 /*
 * Error404 Controller
 */
+var  paths = require($.paths),
+    commonTreeTemplate = require( paths.models + '/commonTreeTemplate.js'),
+    tempModel = require(paths.models + '/template.js');
 
-exports.exec = function(support, errorType) {
-    var mustache = $.require('mustache');
-    //var output = mustache.render(content, {'name': query.name });
+exports.exec = function(support,errorType) {
+    var queriesTemp = {
+	title : "404 - Chere de prince",
+	lang: "fr",
+	sessionDisplay: typeof support.session.user != "undefined",
+	userName:  (typeof support.session.user != "undefined")? support.session.user.name : '',
+	cssLinked:[],
+	jsLinked:[],
+	cssSpe: false
+    };
 
-    support.res.setHeader('Cache-Control','max-age=' + support.page.maxAge );
-    support.res.setHeader("Content-Type", "text/html");
-    $.require('makeTextResponse').send(support.file.html.error404, support.headers, support.res);
+    var section ={
+	id: "section",
+	type: "part",
+	children:{},
+	queries: {
+	    banniereHeader: {
+		link: "/svg/bannieres/404.svg",
+		alt: "La Bécasse en soucoupe volante !!! "
+	    },
+	    jsSpe: false
+	},
+	content: support.file.html.error404
+    };
+
+    commonTreeTemplate.constructTree( queriesTemp, function(tree){
+	tree.children.section = section;
+	tempModel.constructOutput(tree, function(output){
+	    support.res.setHeader('Cache-Control','max-age=' + support.page.maxAge + ',public');
+	    support.res.setHeader('Content-Type', 'text/html');
+	    support.res.statusCode = 404;
+	    $.require('makeTextResponse').send(output, support.headers, support.res);
+	});
+    });
 } ;
