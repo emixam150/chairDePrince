6af856d34d56025797db75822f82700c436eca21
emixam150 <emixam150@gmail.com>
2014-12-12 01:21:03 +0100

    amélioration affichage de graphe

--- home/website/web/io/graph_math.js
+++ home/website/web/io/graph_math.js
@@ -6,84 +6,82 @@ MathOb = require(paths.models + '/math.js')
 
 exports.exec = function(socket){
 
-    socket.on('graphMath', function(type){
+    socket.on('reqMB', function(req){
 
 	var MathBrowser = new MathOb(),
-	limitSize = 1000,
-	query =(type)? {'content.type': type}
-	:{},
+	limitSize = 1997,
 	projection = {name: 1, 
 		      'content.parents': 1, 
 		      'content.children': 1,
 		      'content.title': 1,
 		      'content.type': 1
 		     };
+	buildQuery(req, function(query){
 
-	MathBrowser.findPlus(query, projection, { lastUpdate: 1 }, limitSize, function(docs){
-
-	    preConstructGraph(docs, function(graphObject){
-		socket.emit('dataGraphMath',graphObject);
-	    }); //end of constructGraphObjet
-	}); //end of MathOb.findSort
-    }); //end of onGraphMath
+	    MathBrowser.findPlus(query, projection, { lastUpdate: 1 }, limitSize, function(docs){
+		responseCreator(docs, function(res){
+		    socket.emit('resMB',res);
+		}); //end of constructGraphObjet
+	    }); //end of MathOb.findSort
+	}); //end of onGraphMath
+    }); //end of buildQuery
 }
 
-function preConstructGraph(docs,cb){
-    var cpt = 0,
-    nameFromId ={}
 
+function buildQuery(req,cb){
+    var query ={};
+    query['content.type'] = {$in: req.types};
+    console.log(query);
+    cb(query);
+}
+
+function responseCreator(docs, cb){
+    var res = {}, nameFromId = {}, cpt = 0, mathStyle = new MathOb();
+    
     docs.forEach(function(mathElt){
-	nameFromId[mathElt._id] = mathElt.name
-	cpt++
+	nameFromId[String(mathElt._id)] = mathElt.name
+	res[mathElt.name] = {}
+	res[mathElt.name].title = mathElt.content.title
+	res[mathElt.name].type = mathStyle.translateTypeColor(mathElt.content.type)
+	res[mathElt.name].oldChildren = mathElt.content.children
+	res[mathElt.name].children = [];
+	cpt++;
 	if(docs.length == cpt)
-	    constructGraphObject(docs,nameFromId,cb)
+	    responseCleaner(res,nameFromId,cb)
     })
     if(docs.length == 0)
-	constructGraphObject(docs,nameFromId,cb)
+	cb(res);
+
+//    setTimeout(function(){cb(res)},10000);
 }
 
-function constructGraphObject(docs,nameFromId, cb){
-    var graphObject ={
-	nodes: [],
-	edges:[]
-    },
-    cpt= 0,
-    mathStyle = new MathOb();
+function responseCleaner(res, nameFromId, cb){
     
-    docs.forEach(function(mathElt){
-	graphObject.nodes.push({
-	    id: mathElt.name,
-	    label: mathElt.content.title,
-	    x: Math.random(),
-	    y: Math.random(),
-	    size: mathElt.content.children.length+1,
-	    color: mathStyle.translateTypeColor(mathElt.content.type)
-	});
-	var subcpt = 0;
-	mathElt.content.children.forEach(function(childId){
+    var cpt= 0;
+    
+    Object.keys(res).forEach(function(key){
+	checkGenerations(res[key],nameFromId ,function(){
+	    cpt ++;
+	    if(Object.keys(res).length == cpt)
+		cb(res)
+	})
+    }); // end of forEach mathCont
+}
 
-	    if(nameFromId[childId])
-		graphObject.edges.push({
-		    id: nameFromId[childId] +'-'+ mathElt.name,
-		    source : nameFromId[childId],
-		    target: mathElt.name,
-		    size: 1,
-		    color: mathStyle.translateTypeColor(mathElt.content.type)
-		})
-	    subcpt ++;
-	    if(mathElt.content.children.length == subcpt)
-		next();
-	}); // end of forEach children
-	
-	if(mathElt.content.children.length == 0)
-	    next()
-    }); // end of forEach docs
-    
-    function next(){
-	cpt ++;
-	if(cpt == docs.length)
-	    cb(graphObject);
-    }
-    if(docs.length == 0)
-	cb(graphObject);
-};
+function checkGenerations(content, nameFromId, cb){
+    var cpt = 0;
+    content.oldChildren.forEach( function(childId,index){
+	if(typeof nameFromId[String(childId)] != 'undefined')
+	    content.children.push(nameFromId[String(childId)]);
+	cpt++
+	if(cpt == content.oldChildren.length){
+	    console.log(content.children);
+	    
+	    cb();
+	}
+    })
+    if(content.children.length == 0){
+	cb()}
+}
+
+
--- home/website/web/js/spe/math-browser.js
+++ home/website/web/js/spe/math-browser.js
@@ -1,27 +1,69 @@
 window.onload = function(){
-    var socket = io.connect();
+
+
+
+    var Ctrl = function(){
+	var socket = io.connect();
+	var graph = new Graph();
+	var typesForm = document.getElementById('type'), listResult = document.getElementById('list-result');
+	var ctrl = this;
+	this.res = {}
+
+	this.init = function(){
+	    //init types
+	    for(var i=0; i<typesForm.length;i++){
+		typesForm[i].checked = true;
+	    }
+	    socket.emit('reqMB', {types: ['def','th','prop','cor','axiom','lem','conj'] });
+	    
+	    socket.on('resMB', function(res){
+		ctrl.res = res;
+		ctrl.updateList(res)
+		graph.update(res)
+	    })
+	}
+	//gestion de la selection du type
+	
+	this.typeCtrl = function(){
+	    graph.killLayout();
+	    var typesList =[];
+	    for(var i=0; i<typesForm.length;i++){
+		if(typesForm[i].checked)
+		    typesList.push(typesForm[i].value);
+	    }
+	    socket.emit('reqMB', {types: typesList });
+	}
+	typesForm.onchange = this.typeCtrl; 
+
+	//list result
+
+	this.updateList = function(res){
+	    for(var i=0; i<listResult.children.length; i++){
+		if(res[listResult.children[i].getAttribute('data-name')])
+		    listResult.children[i].style.display = '';
+		else
+		    listResult.children[i].style.display = 'none';
+	    }
+	}
+
+	//roots 
+	
+	//this.rootsC
+    }
     
 
 
     var Graph = function(){
-	var object = this
+	var graph = this;
 	this.graphLoaded = false
 	this.graphData = {}
 
-	socket.emit('graphMath');
-	var s = new sigma('sigma-container');
-	
-	socket.on('dataGraphMath', function(data){
-	    if(!object.graphLoaded){
-		object.graphLoaded = true
-		object.graphData = data
-		object.graphNow = data
-	    }
-	    console.log(data);
-	    update(data)
-	})// end of onDataGraphMath
 
-	var update = function(data){
+	var s = new sigma('sigma-container');
+
+	this.update = function(data){
+	    var dataGraph = builtGraphData(data);
+
 	    // ajout d'events for redirection
 	    s.bind('clickNode rightClickNode', function(e) {
 		window.location.assign('/math/'+e.data.node.id);
@@ -29,63 +71,46 @@ window.onload = function(){
 
 	    // update content of the graph 
 	    s.graph.clear();
-	    s.graph.read(data);
+	    s.graph.read(dataGraph);
 	    s.refresh();
 	    
 	    //gestion of the layout force
-	    s.startForceAtlas2();console.log(s.graph);
+	    s.startForceAtlas2();
 	    setTimeout(function(){s.killForceAtlas2();},120*s.graph.nodes().length);
 	}
 
-	//gestion de la selection du type
-	var typeSelector = document.getElementById('type');
-	
-	typeSelector.onchange = function(){
+	this.killLayout = function(){
 	    s.killForceAtlas2();
-	    socket.emit('graphMath', typeSelector.value);
 	}
 	
-	var listResult = document.getElementById('list-result')
-	    
-
-// 	//gestion de parenté commune
-// 	this.graphNow = this.graphData
-
-// 	for(var k=0 ;k<listResult.children.length;k++){
-// 	    var l =listResult.children[k].getElementsByTagName('input')[0]
-// 	    l.checked = false
-// 	    l.onchange = function(){
-// 		var newData = graphOfChildren("orbite",{edges:[],nodes:[]})
-// 		object.graphNow = newData
-// 		update(newData)
-// 	    }
-// 	}
-	
-// 	var graphOfChildren= function(name,data){
-// 	    var end = true,
-// 		children = []
-// 	    object.graphNow.edges.forEach(function(edge){
-// 		if(edge.souce == name){
-// 		    console.log(name,edge.source)
-// 		    data.edges.push(edge)
-// 		    end = false
-// 		    children.push(edge.target)
-// 		}
-// 	    })
-// 	    if(end){
-// 		data.nodes = object.graphNow.nodes
-// //		console.log(data.edges)
-// 		return data
-		
-// 	    }else{
-// 		children.forEach(function(child){graphOfChildren(child,data)})
-// 	    }
-		
-		
-// 	}
     }
 
-    var graph = new Graph();
+    var ctrl = new Ctrl();
+    ctrl.init();
+
+    function builtGraphData(data){
+	var dataGraph = {nodes:[], edges:[]}
+	Object.keys(data).forEach(function(name){
+	    dataGraph.nodes.push({
+		id: name,
+		label: data[name].title,
+		x: Math.random(),
+		y: Math.random(),
+		size: data[name].children.length +1,
+		color: data[name].type
+	    });
+	    data[name].children.forEach(function(childname){
+		    dataGraph.edges.push({
+			id: childname +'-'+ name,
+			source : childname,
+			target: name,
+			size: 1,
+			color: data[name].type
+		    })
+	    })
+	})
+	return dataGraph;
+    }
 
 } // end of onloadFunction
 
